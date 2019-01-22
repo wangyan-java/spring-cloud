8c58ade62b0aa581b128e55719f5214a3a927b8d
900565 <2637019552@qq.com>
2019-01-22 11:10:29 +0800

    优化 zipkin

--- server_zipkin/src/main/java/com/wy/zipkin/ZipkinApplication.java
+++ server_zipkin/src/main/java/com/wy/zipkin/ZipkinApplication.java
@@ -1,19 +1,17 @@
-package com.wy.configserver;
+package com.wy.zipkin;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
-@EnableEurekaClient
 @Slf4j
-public class ConfigserverApplication {
+public class ZipkinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
+		SpringApplication.run(ZipkinApplication.class, args);
 		log.info("======================启动成功==========================");
 	}
+
 }
+
